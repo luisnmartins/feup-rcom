ca2ab0fb35118e012d0996c2b6f31fe08e653ace
Luís Martins <luis.n.martins97@gmail.com>
2017-10-14 15:34:57 +0100

    fix bug

--- projeto 1/datalink_layer.c
+++ projeto 1/datalink_layer.c
@@ -7,21 +7,20 @@ unsigned char flag_alarm=1;
 unsigned char flag_error=0;
 
 void alarm_handler(){
-	
-  printf("RETRY: %u\n", flag_attempts);
+	flag_attempts++;
+	if(flag_attempts <4){
+		printf("RETRY: %u\n", flag_attempts);
+	}
   if(flag_attempts == 3){
     flag_error = 1;
   }
-  flag_attempts++;
-  flag_alarm=1;
-
+	flag_alarm=1;
 }
 
 
 
 void stateMachine(unsigned char c, int* state, unsigned char* trama){
-	
-	printf("%d\n", *state);
+
 	switch(*state){
 		case S0:
 			if(c == FLAG){
@@ -37,49 +36,48 @@ void stateMachine(unsigned char c, int* state, unsigned char* trama){
 		case S2:
 			if(c != FLAG){
 				*state = S3;
-				trama[1] = c;				
+				trama[1] = c;
 			}
 			break;
 		case S3:
 			if(c != FLAG){
 				trama[2] = c;
 				if((trama[0]^trama[1]) != trama[2]){
-										
-					*state = S0;			
+
+					*state = S0;
 				}
 				else{
 					*state=S4;
 				}
-								
+
 			}
 			break;
 		case S4:
 			if(c == FLAG){
 				STOP = TRUE;
-        		alarm(0);
+        alarm(0);
+				flag_alarm=0;
 			}
 			else{
 				*state = S0;
 			}
 			break;
 
-			
+
 	}
 }
 
 
 int set_writer(int* fd){
-  
-  (void) signal(SIGALRM, alarm_handler);
 
   unsigned char SET[5] = {FLAG, ADDR, CW, BCCW, FLAG};
+  unsigned char elem;
 	int res;
-  char elem;
   unsigned char trama[3];
   int state=0;
+	(void) signal(SIGALRM, alarm_handler);
   while(flag_attempts < 4 && flag_alarm == 1){
-         
-      res = write(*fd,SET,5);   
+      res = write(*fd,SET,5);
       printf("%d bytes written\n", res);
 
       alarm(3);
@@ -87,12 +85,12 @@ int set_writer(int* fd){
 
     // Wait for UA signal.
 
-      while(STOP == FALSE && flag_alarm == 0){      	
-		res = read(*fd,&elem,1);
-       	if(res >0) {
-          stateMachine(elem, &state, trama);
-       	}
-      }  
+      while(STOP == FALSE && flag_alarm == 0){
+					res = read(*fd,&elem,1);
+       		if(res >0) {
+          		stateMachine(elem, &state, trama);
+       		}
+      }
   }
 
   if(flag_error == 1){
@@ -107,7 +105,7 @@ int set_writer(int* fd){
 }
 
 int set_reader(int* fd){
-  
+
   unsigned char UA[5] = {FLAG, ADDR, CR, BCCR, FLAG};
   char elem;
 	int res;
@@ -123,18 +121,17 @@ int set_reader(int* fd){
   printf("%u%u%u\n",trama[0],trama[1],trama[2]);
 
 	res = write(*fd,UA,5);
-	printf("%u%u%u%u%u\n", UA[0], UA[1], UA[2], UA[3], UA[4]);
-
+	sleep(1);
 	printf("%d bytes written\n", res);
 }
 
 
 
 int LLOPEN(int* fd, char* mode){
-    
+
   if(strcmp(mode,"r") == 0){
     return set_reader(fd);
-	
+
   }
   else if(strcmp(mode,"w") == 0){
     return set_writer(fd);
