0ad55cef74574a0240ec0f08962b80b03248b777
Luís Martins <luis.n.martins97@gmail.com>
2017-12-22 15:31:28 +0000

    merge

--- projeto 2/configurations/tux1/scriptux1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ifconfig eth0 up
-ifconfig eth0 172.16.20.1/24
-route add -net 172.16.21.0/24 gw 172.16.20.254
-route add default gw 172.16.20.254
-route add -net 172.16.20.0/24 gw 0.0.0.0
